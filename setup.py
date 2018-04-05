b8a11d89f3d03d166b6e30ee9eac2de10d61faa3
mikeAdams <mikelovesbooks@gmail.com>
2018-04-05 11:41:48 +0100

    structured for git install

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from distutils.core import setup
+setup(
+  name = 'hierarchyParentInjector',
+  packages = ['hierarchyParentInjector'],
+  version = '0.1',
+  description = 'Inserts additional rows into a V4 csv load file, for hierarcical parents nodes that are missing totals. ',
+  author = 'Michael Adams',
+  author_email = 'michael.adams@ons.gov.uk',
+  url = 'https://github.com/ONS-OpenData/hierarchyParentInjector',
+  download_url = 'https://github.com/ONS-OpenData/hierarchyParentInjector/archive/0.1.tar.gz',
+  keywords = ['extraction', 'hierachies', 'data'],
+  classifiers = [],
+)
